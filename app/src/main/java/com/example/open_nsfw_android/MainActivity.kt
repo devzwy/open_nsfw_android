2ae7f5e851705a8042e6faeac15c65e7c6e70c38
jason <dev_zwy@aliyun.com>
2019-05-14 11:48:03 +0800

    first commit.

---
+++ app/src/main/java/com/example/open_nsfw_android/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.open_nsfw_android
+
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.zwy.nsfw.api.NsfwHelper
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    var nsfwHelper: NsfwHelper? = null;
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //assets 目录下的timg-10.jpeg为正常静态图片  ccc.gif 为动态正常图片 可用作测试
+        val b = BitmapFactory.decodeStream(getResources().getAssets().open("ccc.gif"))
+        iv.setImageBitmap(b)
+        nsfwHelper = NsfwHelper.getInstance(this, true, 1)
+
+
+        bt_.setOnClickListener {
+            //同步识别
+            val nsfwBean = nsfwHelper?.scanBitmapSyn(b)
+            Log.d("demo", nsfwBean.toString())
+            tvv.setText("识别成功：\n\tSFW score : ${nsfwBean?.sfw}\n\tNSFW score : ${nsfwBean?.nsfw}")
+            if (nsfwBean?.nsfw ?: 0f > 0.7) {
+                tvv.text = "${tvv.text.toString()} \n \t - 色情图片"
+            }else
+            {
+                tvv.text = "${tvv.text.toString()} \n \t - 正常图片"
+            }
+//            //异步识别，接口回调识别结果
+//            nsfwHelper?.scanBitmap(b) { sfw, nsfw ->
+//                Log.d("demo", "sfw:$sfw,nsfw:$nsfw")
+//            }
+        }
+    }
+
+}

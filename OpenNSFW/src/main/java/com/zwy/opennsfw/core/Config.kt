e06af9779e93974857233f6d112a625867d09b9f
Jason <dev_zwy@aliyun.com>
2020-06-09 11:42:32 +0800

    优化模型支持GPU加速

--- OpenNSFW/src/main/java/com/zwy/opennsfw/core/Config.kt
+++ OpenNSFW/src/main/java/com/zwy/opennsfw/core/Config.kt
@@ -6,7 +6,7 @@ data class Config(
     /**
      * 是否开启GPU加速
      */
-    var isOpenGPU: Boolean = false,
+    var isOpenGPU: Boolean = true,
     var numThreads: Int = 1,
     var context: Context?
 )

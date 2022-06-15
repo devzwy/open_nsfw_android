9bc15c7acf9b08db71a9218c6850cba3609f674b
devzwy <dev_zwy@aliyun.com>
2022-06-15 14:04:04 +0800

    1022 - from linux idea ~ 2022-06-15 14:04:04

--- app/src/main/java/com/zww/sample/Te.kt
+++ app/src/main/java/com/zww/sample/Te.kt
@@ -10,3 +10,5 @@ data class Tl(val tl1:StrictMath,val tl2:LinkageError)
 data class T1020(val is1020True:StrictMath,val tl2:LinkageError)
 //2022-06-15 14:03:04 add ~
 data class T1021(val is1021True:StrictMath,val tl2:LinkageError)
+//2022-06-15 14:04:04 add ~
+data class T1022(val is1022True:StrictMath,val tl2:LinkageError)

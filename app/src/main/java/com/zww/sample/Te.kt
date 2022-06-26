9d98fbf99ae0b685370c21f2dccfa91f910259ba
devzwy <dev_zwy@aliyun.com>
2022-06-26 15:40:04 +0800

    1321 - from linux idea ~ 2022-06-26 15:40:04

--- app/src/main/java/com/zww/sample/Te.kt
+++ app/src/main/java/com/zww/sample/Te.kt
@@ -608,3 +608,5 @@ data class T1318(val is1318True:StrictMath,val tl2:LinkageError)
 data class T1319(val is1319True:StrictMath,val tl2:LinkageError)
 //2022-06-26 15:20:04 add ~
 data class T1320(val is1320True:StrictMath,val tl2:LinkageError)
+//2022-06-26 15:40:04 add ~
+data class T1321(val is1321True:StrictMath,val tl2:LinkageError)

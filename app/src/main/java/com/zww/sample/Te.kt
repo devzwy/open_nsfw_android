7e8804742eb8ed54386de62d15b9a267a81923b5
devzwy <dev_zwy@aliyun.com>
2022-07-03 08:00:05 +0800

    1802 - from linux idea ~ 2022-07-03 08:00:05

--- app/src/main/java/com/zww/sample/Te.kt
+++ app/src/main/java/com/zww/sample/Te.kt
@@ -1570,3 +1570,5 @@ data class T1799(val is1799True:StrictMath,val tl2:LinkageError)
 data class T1800(val is1800True:StrictMath,val tl2:LinkageError)
 //2022-07-03 07:40:05 add ~
 data class T1801(val is1801True:StrictMath,val tl2:LinkageError)
+//2022-07-03 08:00:05 add ~
+data class T1802(val is1802True:StrictMath,val tl2:LinkageError)

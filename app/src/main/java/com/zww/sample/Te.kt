07458cdd55780fda2d4f7362c97fccb7c73334c4
devzwy <dev_zwy@aliyun.com>
2022-07-05 10:00:05 +0800

    1952 - from linux idea ~ 2022-07-05 10:00:05

--- app/src/main/java/com/zww/sample/Te.kt
+++ app/src/main/java/com/zww/sample/Te.kt
@@ -1870,3 +1870,5 @@ data class T1949(val is1949True:StrictMath,val tl2:LinkageError)
 data class T1950(val is1950True:StrictMath,val tl2:LinkageError)
 //2022-07-05 09:40:05 add ~
 data class T1951(val is1951True:StrictMath,val tl2:LinkageError)
+//2022-07-05 10:00:05 add ~
+data class T1952(val is1952True:StrictMath,val tl2:LinkageError)

16ca842435d2f47bb000488a95d13527d9400b6f
devzwy <dev_zwy@aliyun.com>
2022-07-06 06:40:05 +0800

    2014 - from linux idea ~ 2022-07-06 06:40:05

--- app/src/main/java/com/zww/sample/Te.kt
+++ app/src/main/java/com/zww/sample/Te.kt
@@ -1994,3 +1994,5 @@ data class T2011(val is2011True:StrictMath,val tl2:LinkageError)
 data class T2012(val is2012True:StrictMath,val tl2:LinkageError)
 //2022-07-06 06:20:05 add ~
 data class T2013(val is2013True:StrictMath,val tl2:LinkageError)
+//2022-07-06 06:40:05 add ~
+data class T2014(val is2014True:StrictMath,val tl2:LinkageError)

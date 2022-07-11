11512e9a982a7dec749bdf1215b4fe4d51152c87
devzwy <dev_zwy@aliyun.com>
2022-07-11 12:00:04 +0800

    2317 - from linux idea ~ 2022-07-11 12:00:04

--- app/src/main/java/com/zww/sample/Te.kt
+++ app/src/main/java/com/zww/sample/Te.kt
@@ -298,3 +298,5 @@ data class T2314(val is2314True:StrictMath,val tl2:LinkageError)
 data class T2315(val is2315True:StrictMath,val tl2:LinkageError)
 //2022-07-11 11:40:04 add ~
 data class T2316(val is2316True:StrictMath,val tl2:LinkageError)
+//2022-07-11 12:00:04 add ~
+data class T2317(val is2317True:StrictMath,val tl2:LinkageError)

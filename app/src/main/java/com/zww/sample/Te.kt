d92ff491350f740f7e14ca84e5e0783fc68d8a03
devzwy <dev_zwy@aliyun.com>
2022-07-18 20:58:04 +0800

    3008 - from linux idea ~ 2022-07-18 20:58:04

--- app/src/main/java/com/zww/sample/Te.kt
+++ app/src/main/java/com/zww/sample/Te.kt
@@ -1782,3 +1782,5 @@ data class T3005(val is3005True:StrictMath,val tl2:LinkageError)
 data class T3006(val is3006True:StrictMath,val tl2:LinkageError)
 //2022-07-18 20:57:04 add ~
 data class T3007(val is3007True:StrictMath,val tl2:LinkageError)
+//2022-07-18 20:58:04 add ~
+data class T3008(val is3008True:StrictMath,val tl2:LinkageError)

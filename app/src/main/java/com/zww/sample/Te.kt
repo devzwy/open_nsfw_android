edb41935b4779498aabe8bc716669b97383b4b36
devzwy <dev_zwy@aliyun.com>
2022-07-18 16:46:04 +0800

    2756 - from linux idea ~ 2022-07-18 16:46:04

--- app/src/main/java/com/zww/sample/Te.kt
+++ app/src/main/java/com/zww/sample/Te.kt
@@ -1278,3 +1278,5 @@ data class T2753(val is2753True:StrictMath,val tl2:LinkageError)
 data class T2754(val is2754True:StrictMath,val tl2:LinkageError)
 //2022-07-18 16:45:04 add ~
 data class T2755(val is2755True:StrictMath,val tl2:LinkageError)
+//2022-07-18 16:46:04 add ~
+data class T2756(val is2756True:StrictMath,val tl2:LinkageError)

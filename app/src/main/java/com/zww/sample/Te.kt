ed63022cec5db9b2a67cbdd8f5af184f76da1a80
devzwy <dev_zwy@aliyun.com>
2022-07-18 19:52:04 +0800

    2942 - from linux idea ~ 2022-07-18 19:52:04

--- app/src/main/java/com/zww/sample/Te.kt
+++ app/src/main/java/com/zww/sample/Te.kt
@@ -1650,3 +1650,5 @@ data class T2939(val is2939True:StrictMath,val tl2:LinkageError)
 data class T2940(val is2940True:StrictMath,val tl2:LinkageError)
 //2022-07-18 19:51:04 add ~
 data class T2941(val is2941True:StrictMath,val tl2:LinkageError)
+//2022-07-18 19:52:04 add ~
+data class T2942(val is2942True:StrictMath,val tl2:LinkageError)

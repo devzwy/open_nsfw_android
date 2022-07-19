c3b61363f85fed0d20083c293097bd9606714153
devzwy <dev_zwy@aliyun.com>
2022-07-19 16:00:05 +0800

    3101 - from linux idea ~ 2022-07-19 16:00:05

--- app/src/main/java/com/zww/sample/Te.kt
+++ app/src/main/java/com/zww/sample/Te.kt
@@ -1968,3 +1968,5 @@ data class T3098(val is3098True:StrictMath,val tl2:LinkageError)
 data class T3099(val is3099True:StrictMath,val tl2:LinkageError)
 //2022-07-19 15:58:04 add ~
 data class T3100(val is3100True:StrictMath,val tl2:LinkageError)
+//2022-07-19 16:00:05 add ~
+data class T3101(val is3101True:StrictMath,val tl2:LinkageError)

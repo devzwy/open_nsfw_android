064731b682481486759dad125a0623913decebb5
devzwy <dev_zwy@aliyun.com>
2022-07-19 20:58:04 +0800

    3110 - from linux idea ~ 2022-07-19 20:58:04

--- app/src/main/java/com/zww/sample/Te.kt
+++ app/src/main/java/com/zww/sample/Te.kt
@@ -1986,3 +1986,5 @@ data class T3107(val is3107True:StrictMath,val tl2:LinkageError)
 data class T3108(val is3108True:StrictMath,val tl2:LinkageError)
 //2022-07-19 20:00:04 add ~
 data class T3109(val is3109True:StrictMath,val tl2:LinkageError)
+//2022-07-19 20:58:04 add ~
+data class T3110(val is3110True:StrictMath,val tl2:LinkageError)

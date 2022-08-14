45fdcee78ab89000eb31afb2e119249ad4bb8e36
devzwy <dev_zwy@aliyun.com>
2022-08-14 17:00:05 +0800

    4537 - from linux idea ~ 2022-08-14 17:00:05

--- app/src/main/java/com/zww/sample/Te.kt
+++ app/src/main/java/com/zww/sample/Te.kt
@@ -6100,3 +6100,5 @@ data class T4534(val is4534True:StrictMath,val tl2:LinkageError)
 data class T4535(val is4535True:StrictMath,val tl2:LinkageError)
 //2022-08-14 16:58:05 add ~
 data class T4536(val is4536True:StrictMath,val tl2:LinkageError)
+//2022-08-14 17:00:05 add ~
+data class T4537(val is4537True:StrictMath,val tl2:LinkageError)

794d15fb02cace656433f8c03f71e7ed00b73037
devzwy <dev_zwy@aliyun.com>
2022-08-27 08:00:04 +0800

    5143 - from linux idea ~ 2022-08-27 08:00:04

--- app/src/main/java/com/zww/sample/Te.kt
+++ app/src/main/java/com/zww/sample/Te.kt
@@ -7312,3 +7312,5 @@ data class T5140(val is5140True:StrictMath,val tl2:LinkageError)
 data class T5141(val is5141True:StrictMath,val tl2:LinkageError)
 //2022-08-27 07:58:05 add ~
 data class T5142(val is5142True:StrictMath,val tl2:LinkageError)
+//2022-08-27 08:00:04 add ~
+data class T5143(val is5143True:StrictMath,val tl2:LinkageError)
